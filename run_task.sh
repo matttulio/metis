64e12a42e4c229c1a952e230dd5e2f1d8686b881
matttulio <101868697+matttulio@users.noreply.github.com>
2025-04-02 17:51:18 +0200

    Update SLURM job configuration to change account specification

--- run_task.sh
+++ run_task.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=JOB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=matteo.gallo@phd.units.it
-#SBATCH --account=matteo.gallo_26354
+#SBATCH --account=mgallo02
 #SBATCH --partition=g100_all_serial
 #SBATCH --qos=noQOS
 #SBATCH --nodes=1
