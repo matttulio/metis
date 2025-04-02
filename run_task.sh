384454ce662dd47b1fe1c56388d20c2d6c8696a3
matttulio <101868697+matttulio@users.noreply.github.com>
2025-04-02 17:25:10 +0200

    Add GPU computation script and SLURM job configuration

---
+++ run_task.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=JOB
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=Matteo.gallo@phd.units.it
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus=1
+#SBATCH --mem=16G
+#SBATCH --time=12:00:00
+#SBATCH --output=./log_opt/%x.o%A-%a
+#SBATCH --error=./log_opt/%x.o%A-%a
+
+
+
+
+NOW=`date +%H:%M-%a-%d/%b/%Y`
+echo '------------------------------------------------------'
+echo 'This job is allocated on '$SLURM_JOB_CPUS_PER_NODE' cpu(s)'
+echo 'Job is running on node(s): '
+echo  $SLURM_JOB_NODELIST
+echo '------------------------------------------------------'
+echo 'WORKINFO:'
+echo 'SLURM: job starting at           '$NOW
+echo 'SLURM: sbatch is running on      '$SLURM_SUBMIT_HOST
+echo 'SLURM: executing on cluster      '$SLURM_CLUSTER_NAME
+echo 'SLURM: executing on partition    '$SLURM_JOB_PARTITION
+echo 'SLURM: working directory is      '$SLURM_SUBMIT_DIR
+echo 'SLURM: current home directory is '$(getent passwd $SLURM_JOB_ACCOUNT | cut -d: -f6)
+echo ""
+echo 'JOBINFO:'
+echo 'SLURM: job identifier is         '$SLURM_JOBID
+echo 'SLURM: job name is               '$SLURM_JOB_NAME
+echo ""
+echo 'NODEINFO:'
+echo 'SLURM: number of nodes is        '$SLURM_JOB_NUM_NODES
+echo 'SLURM: number of cpus/node is    '$SLURM_JOB_CPUS_PER_NODE
+echo 'SLURM: number of gpus/node is    '$SLURM_GPUS_PER_NODE
+echo '------------------------------------------------------'
+
+
+python -u try.py
---
+++ try.py
@@ -0,0 +1,10 @@
+import jax
+import jax.numpy as jnp
+
+# Run a simple computation on the GPU
+x = jnp.ones((1000, 1000))
+y = jax.device_put(x)  # Send data to the GPU
+z = y * 2
+
+# Print the device
+print(f"Computation done on: {z.device()}")
