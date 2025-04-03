0627637fd289322fbf8bd23fdd3a29171d83fa44
matttulio <101868697+matttulio@users.noreply.github.com>
2025-04-03 10:38:58 +0200

    Update SLURM job configuration to change QoS setting and remove memory allocation

--- run_task.sh
+++ run_task.sh
@@ -4,14 +4,11 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=matteo.gallo@phd.units.it
 #SBATCH --partition=boost_usr_prod
-#SBATCH --qos=boost_qos_dbg
+#SBATCH --qos=normal
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=16G
 #SBATCH --time=00:15:00
-#SBATCH --output=./log_opt/%x.o%A-%a
-#SBATCH --error=./log_opt/%x.o%A-%a
 
 
 
