ea4e9ab038af964af5c463d204b29752b6618d81
matttulio <101868697+matttulio@users.noreply.github.com>
2025-04-03 10:36:42 +0200

    Update SLURM job configuration with new partition and QoS settings, and adjust time limit

--- run_task.sh
+++ run_task.sh
@@ -3,13 +3,13 @@
 #SBATCH --job-name=JOB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=matteo.gallo@phd.units.it
-#SBATCH --partition=lrd_all_serial
-#SBATCH --qos=noQOS
+#SBATCH --partition=boost_usr_prod
+#SBATCH --qos=boost_qos_dbg
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=16G
-#SBATCH --time=2:00:00
+#SBATCH --time=00:15:00
 #SBATCH --output=./log_opt/%x.o%A-%a
 #SBATCH --error=./log_opt/%x.o%A-%a
 
