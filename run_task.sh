5c8f8cfee9da5178d92635eeee2c32085b202f8f
matttulio <101868697+matttulio@users.noreply.github.com>
2025-04-03 14:36:01 +0200

    no changes

--- run_task.sh
+++ run_task.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=JOB
 #SBATCH --partition=boost_usr_prod
 #SBATCH --qos=boost_qos_dbg
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=00:15:00
